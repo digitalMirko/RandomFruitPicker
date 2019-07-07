66fca675f22b608975ac12cd651b839456753ced
Mirko Cukich <mirkocukich@gmail.com>
2019-07-06 23:52:25 -0500

    IBOutlet and IBAction connections made on ViewController

--- Random Fruit Picker/ViewController.swift
+++ Random Fruit Picker/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Top title and fruit name text label
+    @IBOutlet weak var nameLabel: UILabel!
+    // Fruit image in the middle
+    @IBOutlet weak var fruitImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    // Picker button
+    @IBAction func pickButton(_ sender: Any) {
+        
+        
+    }
+    
 }
 
